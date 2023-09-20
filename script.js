710074699996ff433443e1802bcab4c67f14e480
RomaSamkov <Romansamkov.rs@gmail.com>
2023-09-20 20:55:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,5 +37,23 @@ function testEven(n) {
   return n % 2 === 0 ? false : true;
 }
 
-console.log(testEven(0));
-console.log(testEven(1));
+// console.log(testEven(0));
+// console.log(testEven(1));
+
+function oddOrEven(array) {
+  if (array.length === 0) {
+    return "even";
+  }
+
+  const ar = [...array].reduce((total, item) => total + item);
+  if (ar % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+  // return ar % 2 === 0 ? "even" : "odd";
+}
+
+console.log(oddOrEven([0]));
+console.log(oddOrEven([0, 1, 5]));
+console.log(oddOrEven([0, 1, 2]));
