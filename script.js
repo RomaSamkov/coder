15394111501f8becb81042e1fd56978980d5568d
RomaSamkov <Romansamkov.rs@gmail.com>
2023-10-03 16:30:26 +0300

    add function correct

--- script.js
+++ script.js
@@ -157,4 +157,23 @@ function getAge(inputString) {
   return Number(inputString.charAt(0));
 }
 
-console.log(getAge("4 years old"));
+// console.log(getAge("4 years old"));
+
+function correct(string) {
+  string = string.split("");
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] == "5") {
+      string[i] = "S";
+    }
+    if (string[i] == "0") {
+      string[i] = "O";
+    }
+    if (string[i] == "1") {
+      string[i] = "I";
+    }
+  }
+  return string.join("");
+}
+
+console.log(correct("L0ND0N"));
+console.log(correct("DUBL1N"));
