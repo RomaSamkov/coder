b7025078aa4726d02cb4c28d8d6417697ce82366
RomaSamkov <Romansamkov.rs@gmail.com>
2023-10-04 15:42:27 +0300

    add function points

--- script.js
+++ script.js
@@ -175,5 +175,25 @@ function correct(string) {
   return string.join("");
 }
 
-console.log(correct("L0ND0N"));
-console.log(correct("DUBL1N"));
+// console.log(correct("L0ND0N"));
+// console.log(correct("DUBL1N"));
+
+function points(games) {
+  let x = 0;
+  games.map((item) => {
+    if (item[0] > item[2]) {
+      x = x + 3;
+    } else if (item[0] == item[2]) {
+      x = x + 1;
+    }
+  });
+  return x;
+}
+
+console.log(
+  points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"])
+);
+
+console.log(
+  points(["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"])
+);
