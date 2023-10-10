1061715f81b26e9f52a7986e985e6b1405d84c21
RomaSamkov <Romansamkov.rs@gmail.com>
2023-10-10 20:30:34 +0300

    add function findNeedle

--- script.js
+++ script.js
@@ -218,4 +218,21 @@ function upperCase(string) {
   return string.toString() === string.toUpperCase();
 }
 
-console.log(upperCase("Hello World!"));
+// console.log(upperCase("Hello World!"));
+
+function findNeedle(haystack) {
+  const index = haystack.indexOf("needle");
+  return `found the needle at position ${index}`;
+}
+
+console.log(
+  findNeedle([
+    "283497238987234",
+    "a dog",
+    "a cat",
+    "some random junk",
+    "a piece of hay",
+    "needle",
+    "something somebody lost a while ago",
+  ])
+);
