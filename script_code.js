f74352bf9c2d7dd74035d693291d71981faf5c5d
RomaSamkov <Romansamkov.rs@gmail.com>
2023-11-22 18:11:42 +0200

    add function litres

--- script_code.js
+++ script_code.js
@@ -2,4 +2,10 @@ function opposite(number) {
   return -number;
 }
 
-console.log(opposite(4));
+// console.log(opposite(4));
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+console.log(litres(11.8));
