c47140c62057fd0e37c1a411a5fb65e1de5e35b2
RomaSamkov <Romansamkov.rs@gmail.com>
2023-11-23 16:51:14 +0200

    function findMultiples

--- script_code.js
+++ script_code.js
@@ -8,4 +8,14 @@ function litres(time) {
   return Math.floor(time * 0.5);
 }
 
-console.log(litres(11.8));
+// console.log(litres(11.8));
+
+function findMultiples(integer, limit) {
+  let result = [];
+  for (let i = integer; i <= limit; i += integer) {
+    result.push(i);
+  }
+  return result;
+}
+
+console.log(findMultiples(5, 25));
