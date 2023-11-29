9d7f1c7ac73be5767da21136ac16efe3e42d093e
RomaSamkov <Romansamkov.rs@gmail.com>
2023-11-29 12:41:54 +0200

    add function min and function max

--- script_code.js
+++ script_code.js
@@ -65,4 +65,15 @@ function peopleWithAgeDrink(old) {
   }
 }
 
-console.log(peopleWithAgeDrink(21));
+// console.log(peopleWithAgeDrink(21));
+
+function min(list) {
+  return Math.min(...list);
+}
+
+function max(list) {
+  return Math.max(...list);
+}
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110])); /*  -110  */
+console.log(max([4, 6, 2, 1, 9, 63, -134, 566])); /* 566 */
