48002eba5715c8d515a706296678a4f2be4c5c46
RomaSamkov <Romansamkov.rs@gmail.com>
2023-12-19 17:24:18 +0200

    add function switchItUp

--- script_code.js
+++ script_code.js
@@ -284,4 +284,47 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
 
-console.log(twiceAsOld(36, 7));
+// console.log(twiceAsOld(36, 7));
+
+function switchItUp(number) {
+  switch (number) {
+    case 0:
+      return "Zero";
+      break;
+    case 1:
+      return "One";
+      break;
+    case 2:
+      return "Two";
+      break;
+    case 3:
+      return "Three";
+      break;
+    case 4:
+      return "Four";
+      break;
+    case 5:
+      return "Five";
+      break;
+    case 6:
+      return "Six";
+      break;
+    case 7:
+      return "Seven";
+      break;
+    case 8:
+      return "Eight";
+      break;
+    case 9:
+      return "Nine";
+      break;
+    case 10:
+      return "Ten";
+      break;
+
+    default:
+      break;
+  }
+}
+
+console.log(switchItUp(6));
