ba373d64ca11be85fae99d1152667ed054faa4b2
RomaSamkov <Romansamkov.rs@gmail.com>
2023-12-21 20:42:34 +0200

    add function solve

--- script_code.js
+++ script_code.js
@@ -327,4 +327,20 @@ function switchItUp(number) {
   }
 }
 
-console.log(switchItUp(6));
+// console.log(switchItUp(6));
+
+function solve(s) {
+  let lowerCase = 0;
+  let upperCase = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i].toUpperCase()) {
+      upperCase++;
+    } else {
+      lowerCase++;
+    }
+  }
+  return upperCase > lowerCase ? s.toUpperCase() : s.toLowerCase();
+}
+
+console.log(solve("CODe"));
+console.log(solve("Code"));
