0d4e478f4e4b7448e06836cb5924e3c59eee3d08
RomaSamkov <Romansamkov.rs@gmail.com>
2023-12-24 17:07:54 +0200

    add function greet

--- script_code.js
+++ script_code.js
@@ -367,4 +367,29 @@ function persistence(num) {
   return counter;
 }
 
-console.log(persistence(39));
+// console.log(persistence(39));
+
+function greet(language) {
+  const database = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+  return database[language] || database["english"];
+}
+
+console.log(greet("spanish"));
