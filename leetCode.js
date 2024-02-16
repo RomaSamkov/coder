283304d628b20e645d80f44944e8263878360d13
RomaSamkov <Romansamkov.rs@gmail.com>
2024-02-16 12:16:24 +0200

    add function chunk

--- leetCode.js
+++ leetCode.js
@@ -392,3 +392,15 @@ var promiseAll = function (functions) {
     }
   });
 };
+
+var chunk = function (arr, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < arr.length) {
+    chunked.push(arr.slice(index, size + index));
+    index += size;
+  }
+
+  return chunked;
+};
