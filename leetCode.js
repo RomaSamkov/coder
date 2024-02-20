2743ed7e7bd87d00fd727e915a41cea9df5486e6
RomaSamkov <Romansamkov.rs@gmail.com>
2024-02-20 11:59:27 +0200

    add function join

--- leetCode.js
+++ leetCode.js
@@ -439,3 +439,19 @@ Array.prototype.groupBy = function (fn) {
 var sortBy = function (arr, fn) {
   return arr.sort((a, b) => fn(a) - fn(b));
 };
+
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+const join = function (arr1, arr2) {
+  const ob = {};
+  for (const i of arr1) {
+    ob[i.id] = i;
+  }
+  for (const i of arr2) {
+    ob[i.id] = ob[i.id] ? { ...ob[i.id], ...i } : i;
+  }
+  return Object.values(ob);
+};
